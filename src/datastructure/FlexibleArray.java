aa0c0b472817602c7de990143c5af453266a8f79
justoj <justojrb05@gmail.com>
2025-11-21 11:37:11 +0100

    ventajas y desventajas de un array

--- src/datastructure/FlexibleArray.java
+++ src/datastructure/FlexibleArray.java
@@ -1,77 +1,84 @@
 package datastructure;
 
-/**
+/*
 This is a class that represent an extensible array.
 An array has a fixed capacity, when reaching the end
 of the array, we have to expand it.
- * We use this constructor to create an array when we know
- * how many elements to store in the array
- * @param "size"
  */
 
-public class FlexibleArray<T> {
-<<<<<<< HEAD
-    private static final int EXPANSION_FACTOR = 40;
-=======
->>>>>>> dcc151a00a04fb1ae123c7b5d4a6f7d7e8c8e5bd
-    //An array with fixed size
-    private T [] elements;
-    private static final int  INITIAL_CAPACITY = 5;
-    //the variable to store how many elements we have added to the array
-    private int size =0;
-
-    /**
-     * we use this constructor to create an array when we know
-     * how many elements to store in the array
-     * @param size
+public class FlexibleArray<T>{
+    /*
+    We use this constructor to create an array when
+    we know how many elements to store in the array
      */
-    public FlexibleArray(int size){
-<<<<<<< HEAD
-        if(size <= 0){
+    //An array with fixed size
+    private T[] elements;
+    private final int INITIAL_CAPACITY = 5;
 
-        }
-=======
->>>>>>> dcc151a00a04fb1ae123c7b5d4a6f7d7e8c8e5bd
-        elements = (T[])new Object [size];
+    //The variable to store how many elements we have added to the array
+    private int size = 0;
+    private static final int EXPANSION_FACTOR = 40;
+    public FlexibleArray(int size){
+    	if(size <= 0) {
+    		size = INITIAL_CAPACITY;
+    	}
+    	//create an array of objects
+        elements = (T[]) new Object[size];
     }
-    /**
-     * Use this constructor to create an array with a default capacity.
+
+    /*
+    Use this constructor to create an array with a default capacity
      */
     public FlexibleArray(){
-        elements = (T[])new Object [INITIAL_CAPACITY];
-
-
+        elements = (T[]) new Object[INITIAL_CAPACITY];
     }
-    /**
-     *add an element to the array at its last partition
-     * @param t
+
+    /*
+    Add an element to the array at its last position
      */
-    public void add(T t) {
-<<<<<<< HEAD
-        elements[size] = t;
-        //paso 0 : check if we have reached to the last position,
+
+    public void add(T e) {
+        //paso 0: check if we have reached to the last position,
         //if true, we have to expand the capacity of the array
+    	if(size == elements.length) {//The array is full, we have to extend its capacity    		
+    		//First, we create a new array with mayor capacity
+    		T[] newArr = (T[])new Object[elements.length + EXPANSION_FACTOR];
+    		// copy the elements from the old array to the new one
+    		//newArr[0] = elements[1]
+    		//newArr[0] = elements[1]
+    		for(int i = 0; i < elements.length; i++) {
+    			newArr[i] = elements[i];
+    		}
+    		elements = newArr;
+    	}
         //paso 1: add the element to the last position of the array
-        if(size == elements.length) {
-            //first we create a new array with mayor capacity
-            T[] newArr = (T[])new Object[elements.length + EXPANSION_FACTOR];
-        }
+    	elements[size] = e;
+    	size++;
+        //paso 2:
     }
 
-    public void remove(int t) {
-    	elements[t] = null;
+    /*
+    Remove the element at position "i"
+     */
+
+    public void remove(int i) {
+    	//Step 1: eliminate the element at position passed by the parameter
+    	elements[i] = null;
+//    	elements[i] = elements[i + 1];
+//    	elements[i + 1] = elements[i + 2];
+//    	elements[i + 2] = elements[i + 3];
+//    	elements[i + 3] = elements[i + 4];
+    	//...
+    	
+    	for (int j = 0; j < 5; j++) {
+			T e = elements[j];
+		}
+    	
     	size--;
-=======
-        //paso 0 : check if we have reached to the last position,
-        //if true, we have to expand the capacity of the array
-        //paso 1: add the element to the last position of the array
     }
 
-    public void remove(int t) {
->>>>>>> dcc151a00a04fb1ae123c7b5d4a6f7d7e8c8e5bd
-    }
-
-    public int size (){
-        return 0;
+    public int size() {
+        return size;
     }
 }
+    
--- src/datastructure/FlexibleArrayTest.java
+++ src/datastructure/FlexibleArrayTest.java
@@ -5,7 +5,6 @@ package datastructure;
 public class FlexibleArrayTest {
     public static void main(String[] args) {
         FlexibleArray<Integer> flexibleArray = new FlexibleArray<>();
-<<<<<<< HEAD
         new FlexibleArray<>(0);
         System.out.println("the current size = " + flexibleArray.size());
 
@@ -13,12 +12,6 @@ public class FlexibleArrayTest {
             flexibleArray.add(3 + i);
         }
         System.out.println("the current size = " + flexibleArray.size());
-
-
-=======
-        System.out.println("the current size = " + flexibleArray.size());
-        flexibleArray.add(3);
->>>>>>> dcc151a00a04fb1ae123c7b5d4a6f7d7e8c8e5bd
         flexibleArray.remove(4);
     }
 }
