cb1fe2e2cf6e55a87cd7667821942420bbcdc571
justoj <justojrb05@gmail.com>
2025-11-26 12:21:14 +0100

    26/11/25

---
+++ src/oopmodeling/addressbook/AddressBook.java
@@ -0,0 +1,31 @@
+package oopmodeling.addressbook;
+
+import java.util.Scanner;
+
+import datastructure.FlexibleArray;
+
+/**
+ * This class is in charge of administrating a group of contacts
+ *@author Justo
+ *26 nov 2025
+ */
+public class AddressBook {
+	
+	//Atributes
+	protected FlexibleArray <Contact> contacts;
+	
+	protected Scanner scanner;
+
+	//Behaves
+	public void showContacts() {
+		
+	}
+	
+	public void addContact(Contact contact) {
+		
+	}
+	
+	public void removeContact(String name) {
+		
+	}
+}
---
+++ src/oopmodeling/addressbook/ContactApp.java
@@ -0,0 +1,15 @@
+package oopmodeling.addressbook;
+
+
+public class ContactApp {
+	
+	public static void main(String[] args) {
+		
+		AddressBook addressbook = new AddressBook();
+		
+		addressbook.showContacts();
+		addressbook.addContact(null);
+
+	}
+
+}
