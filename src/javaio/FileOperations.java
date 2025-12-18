a98db2c0e450bbb4be7a623d85f8049f8a940e2d
justoj <justojrb05@gmail.com>
2025-12-18 10:21:26 +0100

    lector de archivos

--- src/javaio/FileOperations.java
+++ src/javaio/FileOperations.java
@@ -1,131 +1,147 @@
 package javaio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- * 
+/*
+ *
  * @author Justo
- * 16 dic 2025
+ * 18 dic 2025
  */
-
 public class FileOperations {
+	
+	public static void main(String[] args) {
+		
+		File file = new File("/logs/user.log");
+		File directory = new File("/manolo/logs/");
+		System.out.println("Working directory: " + System.getProperty("user.dir"));
+		
+		createPath(directory);
+		createFile(file);	
+		createAbsolutePath();
+		writeFile();
+		readFile();
+		bufferedRead();
+		bufferedWrite();
+		
+	}
 
-    public static void main(String[] args) {
+	private static void bufferedWrite() {
+		
+	}
 
-        File file = new File("log.txt");
-        File directory = new File("logs");
+	private static void bufferedRead() {
+		try {
+			System.out.println("Buffered reader");
+			BufferedReader br = new BufferedReader(new FileReader("log.txt"));
+			System.out.println(br.readLine());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println();
+	}
 
-        // Show current working directory
-        System.out.println("working directory : " + System.getProperty("user.dir"));
+	private static void readFile() {		
+		try {
+			FileReader reader = new FileReader("log.txt");
+			//System.out.print((char)reader.read());
+			
+			//check the condition first before executing the while body
+			int c;
+			//System.out.println((char) -1 == -1);
+			System.out.println("\n Primera lectura");
+			//If we have read "-1", the loop finishes
+			while ((c = reader.read()) != -1) {
+				System.out.print((char)c);
+			}		
+			reader.close();
+			
+			reader = new FileReader("log.txt");
+			System.out.println("\n \n Segunda lectura");
+			//read a character from the file
+			c = reader.read();
+			while(c != -1) { //check if we have reached the end of the file
+				//haven't reached the end
+				System.out.print((char)c);
+				c = reader.read();
+			}
+			reader.close();
+			
+			reader = new FileReader("log.txt");
+			System.out.println("\n \n Tercera lectura");
+			//Execute the commands inside the "do" body first, 
+			//then check the condition if it is true, repeat 
+			//the execution	
+			c = 0;
+			System.out.println("El valor de c es " + c);
+			do {
+				c = reader.read();
+				System.out.print((char)c);
+			}			
+			while (c != -1);
+			reader.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-        createPath(directory);
-        createFile(file);
-        createAbsolutePath();
+	private static void writeFile() {
+		try {
+			FileWriter writer = new FileWriter("log.txt");
+			writer.write("Black big dildo ");
+			writer.write("A Panblo le gusta el pene negro ");
+			writer.write('Y');
+			//Close the writer and refresh the data to the file
+			writer.close();
+			//Refresh data we have written to the file
+			//writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-        writeFile();
+	private static void createAbsolutePath() {
+		//create directory using absolute path
+		File absoluteDir = new File(System.getProperty("user.dir") + "/logs/user.log");
+		absoluteDir.mkdirs();
+	}
 
-        // Read file using while and do-while
-        readFile();
-    }
+	private static void createPath(File file) {
+		
+		System.out.println(file.isAbsolute());
+		System.out.println(file.getPath());
+		//create directories specified by the "file" object
+		file.getParentFile().mkdirs();
+		System.out.println(file.getAbsolutePath());
+		
+	}
 
-    private static void readFile() {
-        try {
+	/*
+	 * Create a file 
+	 */
+	
+	private static void createFile(File file) {
+		
+		
+		if(file.exists()) {
+			System.out.println("The log file exists");
+		}
+		else {
+			System.out.println("The log file does not exist");
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			//With try catch, we can continue executing the program without interruption
+			System.out.println("Finshed");
+		}
+		
+	}
 
-            // FileReader reads the file character by character
-            FileReader reader = new FileReader("log.txt");
-
-            /**
-             * check the condition first before executing the while body
-             * FileReader.read() returns an int
-             * -1 means end of file (EOF)
-             */
-            int c;
-            while ((c = reader.read()) != -1) {
-                System.out.print((char) c);
-            }
-
-            // Close reader after finishing while loop
-            reader.close();
-
-            System.out.println("\n--- Reading again using do-while ---");
-
-            // New reader needed because the previous one was closed
-            reader = new FileReader("log.txt");
-
-            /**
-             * Execute the commands inside the "do" body first,
-             * then check the condition, if it is true repeat
-             * the execution
-             */
-            do {
-                c = reader.read();
-                if (c != -1) {
-                    System.out.print((char) c);
-                }
-            } while (c != -1);
-
-            // Close reader after do-while
-            reader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void writeFile() {
-        try {
-
-            FileWriter writer = new FileWriter("log.txt");
-
-            writer.write("Black holes\n");
-            writer.write("pollas en vinagre\n");
-            writer.write("End of file example\n");
-
-            // close the writer and refresh the data
-            writer.close();
-
-            // refresh data we have written to the file
-            // writer.flush();  // not needed if we close the writer
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void createAbsolutePath() {
-
-        // create directory using absolute path
-        File absoluteDir = new File(System.getProperty("user.dir") + "/logs");
-        absoluteDir.mkdir();
-    }
-
-    private static void createPath(File file) {
-
-        System.out.println(file.isAbsolute());
-        System.out.println(file.getPath());
-
-        // create directories specified by the "file" object
-        file.mkdirs();
-
-        System.out.println(file.getAbsolutePath());
-    }
-
-    private static void createFile(File file) {
-
-        if (file.exists()) {
-            System.out.println("the log file exists");
-        } else {
-            System.out.println("the log file does not exist");
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                // With try catch, we can continue executing the program without interruption
-                e.printStackTrace();
-            }
-            System.out.println("finished");
-        }
-    }
 }
