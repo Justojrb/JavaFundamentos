9e645416335a30054bf80e19b3912c2dfa87f244
justoj <justojrb05@gmail.com>
2026-01-14 14:39:37 +0100

    manipular datos binarios

--- src/javaio/BinaryDataOperations.java
+++ src/javaio/BinaryDataOperations.java
@@ -1,30 +1,72 @@
 package javaio;
 
+import java.io.DataInputStream;
 import java.io.DataOutput;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+/**
+ * Write and read binary to and from a file
+ *@author Justo
+ *14 ene 2026
+ */
 
 public class BinaryDataOperations {
 	
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		writeBinaryData();
+		readBinaryData();
 		
 		//Avariable with null initialization
 		//DataOutputStream os = null ;
 		
 		
 		
-try {
-	DataOutputStream os =
-	new DataOutputStream(new FileOutputStream("hola.bat"));
-	os.writeChar(34);
+try (DataOutputStream os =
+new DataOutputStream(new FileOutputStream("hola.txt"));){
+	
+	os.writeChar(01);
+	os.writeInt(10);
+	os.writeUTF("Pablo is MARICON");
 
-
+//os.close();
 } catch (Exception e) {
 	
 	e.printStackTrace();
 	
-   }		
+   }
 
   }
+
+	private static void readBinaryData() throws FileNotFoundException {
+		DataInputStream is = 
+				new DataInputStream(new FileInputStream(""));
+		try {
+			is.readChar();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			is.readInt();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			is.readUTF();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static void writeBinaryData() {
+		
+	}
 	
 }
